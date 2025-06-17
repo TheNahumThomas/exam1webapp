6cc2d9f903744a5f0e7bbcc9590b1d7f445a5d4b
Nahum Thomas <5254664@ADA.com>
2025-06-17 16:32:54 +0100

    committting post exam 2
    :q

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utilities "exam1webapp/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/google/safehtml/template"
@@ -17,8 +18,41 @@ func main() {
 	}))
 
 	mux.Handle("/home", safehttp.MethodGet, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+		authenticated := false
+		username := "Unknown User"
 		t := template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\index.html"))
-		return safehttp.ExecuteTemplate(w, t, nil)
+		userSessionID, err := r.Cookie("session")
+		if err != nil {
+			fmt.Println("Error retrieving cookies:", err)
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+		usernameCookie, err := r.Cookie("username")
+		if err != nil {
+			fmt.Println("Error retrieving cookies:", err)
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		if userSessionID == nil || usernameCookie == nil {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		username = usernameCookie.Value()
+		sessionOnwer, err := utilities.CheckSession(userSessionID.Value())
+		if err != nil {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		if sessionOnwer != username {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		} else {
+			authenticated = true
+		}
+
+		if !authenticated {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		return safehttp.ExecuteTemplate(w, t, map[string]interface{}{"Username": username})
 	}))
 
 	mux.Handle("/login", safehttp.MethodGet, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
@@ -26,9 +60,8 @@ func main() {
 		return safehttp.ExecuteTemplate(w, t, nil)
 	}))
 
-	mux.Handle("/login", safehttp.MethodPost, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+	mux.Handle("/login/attempt", safehttp.MethodPost, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
 		t := template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\login.html"))
-
 		formData, err := r.PostForm()
 		if err != nil {
 			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
@@ -37,19 +70,85 @@ func main() {
 		username := formData.String("username", "")
 		password := formData.String("password", "")
 
-		err = utilities.Login(username, password, w)
+		sessionID, err := utilities.Login(username, password)
+		fmt.Println(err)
 		if err != nil {
-			return safehttp.ExecuteTemplate(w, t, map[string]interface{}{"Error": "Invalid credentials"})
+			return safehttp.ExecuteTemplate(w, t, map[string]interface{}{"Error": "Invalid username or password"})
 		}
 
-		data := map[string]string{"Username": username}
+		sessionCookie := safehttp.NewCookie("session", sessionID)
+		usernameCookie := safehttp.NewCookie("username", username)
+		usernameCookie.Path("/")
+		sessionCookie.Path("/")
 
-		return safehttp.ExecuteTemplate(w, t, data)
+		err = safehttp.ResponseHeadersWriter(w).AddCookie(sessionCookie)
+		if err != nil {
+			return safehttp.ExecuteTemplate(w, t, map[string]interface{}{"Error": "Failed to set session cookies"})
+		}
+		err = safehttp.ResponseHeadersWriter(w).AddCookie(usernameCookie)
+		if err != nil {
+			return safehttp.ExecuteTemplate(w, t, map[string]interface{}{"Error": "Failed to set session cookies"})
+		}
+
+		return safehttp.Redirect(w, r, "/home", safehttp.StatusFound)
+
 	}))
 
-	mux.Handle("/upload", safehttp.MethodGet, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+	mux.Handle("/patient-record", safehttp.MethodGet, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
 		t := template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\upload.html"))
+		userSessionID, err := r.Cookie("session")
+		if err != nil {
+			fmt.Println("Error retrieving cookies:", err)
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+		usernameCookie, err := r.Cookie("username")
+		if err != nil {
+			fmt.Println("Error retrieving cookies:", err)
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		if userSessionID == nil || usernameCookie == nil {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		username := usernameCookie.Value()
+		sessionOnwer, err := utilities.CheckSession(userSessionID.Value())
+		if err != nil {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
+		if sessionOnwer != username {
+			return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+		}
+
 		return safehttp.ExecuteTemplate(w, t, nil)
+	}))
+
+	mux.Handle("/logout", safehttp.MethodGet, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+		sessionReset := safehttp.NewCookie("session", "")
+		sessionReset.Path("/")
+		err := safehttp.ResponseHeadersWriter(w).AddCookie(sessionReset)
+		if err != nil {
+			return safehttp.ExecuteTemplate(w, template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\login.html")), map[string]interface{}{"Error": "Failed to logout"})
+		}
+		return safehttp.Redirect(w, r, "/login", safehttp.StatusFound)
+	}))
+
+	mux.Handle("/upload", safehttp.MethodPost, safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+		formData, err := r.MultipartForm(20000)
+		if err != nil {
+			return safehttp.ExecuteTemplate(w, template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\upload.html")), map[string]interface{}{"Error": "Failed to upload file"})
+		}
+
+		file := formData.File("file")
+		if len(file) == 0 {
+			return safehttp.ExecuteTemplate(w, template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\upload.html")), map[string]interface{}{"Error": "No file uploaded"})
+		}
+
+		file[0].Open()
+
+		return safehttp.ExecuteTemplate(w, template.Must(template.ParseFiles("static\\templates\\base.html", "static\\templates\\upload.html")), map[string]interface{}{"Success": "File uploaded successfully"})
+
 	}))
 
 	http.ListenAndServe(":8080", mux)
--- utils/Login.go
+++ utils/Login.go
@@ -1,17 +1,20 @@
 package utilities
 
 import (
+	"crypto/rand"
 	"database/sql"
-	"errors"
+	"encoding/base64"
+	"fmt"
 
-	"github.com/google/go-safeweb/safehttp"
 	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
 	"golang.org/x/crypto/bcrypt"
 )
 
-const dbFile = "exam1webapp/webapp.db"
+const dbFile = "webapp.db"
 
-const loginQuery = `SELECT password FROM tbl_users WHERE username = ?`
+const loginQuery = "SELECT `password` FROM tbl_users WHERE `username` = ?"
+const sessionStoreQuery = "UPDATE `tbl_users` SET `sessionid` = ? WHERE `username` = ?"
+const checkSessionIDQuery = "SELECT `username` FROM `tbl_users` WHERE `sessionid` = ?"
 
 func dbConnect() (*sql.DB, error) {
 
@@ -23,32 +26,62 @@ func dbConnect() (*sql.DB, error) {
 	return db, nil
 }
 
-// func generateSessionID() (string, error) {
-// 	b := make([]byte, 32)
-// 	_, err := rand.Read(b)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return base64.URLEncoding.EncodeToString(b), nil
-// }
+func generateSessionID() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
 
-func Login(username, password string, w safehttp.ResponseWriter) error {
+func Login(username, password string) (string, error) {
 	var storedPassword string
+
 	db, err := dbConnect()
 	if err != nil {
-		return err
+		return "", err
 	}
+
+	defer db.Close()
 
 	err = db.QueryRow(loginQuery, username).Scan(&storedPassword)
 	if err != nil {
-		w.WriteError(safehttp.StatusUnauthorized)
-		return err
+		return "", err
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(password))
 	if err != nil {
-		return errors.New("invalid credentials")
+		return "", err
 	}
 
-	return nil
+	sessionID, err := generateSessionID()
+	if err != nil {
+		return "", err
+	}
+
+	_, err = db.Exec(sessionStoreQuery, sessionID, username)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(sessionID)
+	return sessionID, nil
+}
+
+func CheckSession(sessionID string) (string, error) {
+	db, err := dbConnect()
+	if err != nil {
+		return "", err
+	}
+
+	var username string
+	err = db.QueryRow(checkSessionIDQuery, sessionID).Scan(&username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", nil
+		}
+		return "", err
+	}
+
+	return username, nil
 }
